466d8b6dff360eb4a277fa44d8798d106e7864b3
keshew <artyom.korotckov2017@yandex.ru>
2025-03-06 15:49:35 +0500

    Initial Commit

---
+++ GirlRecipe/GirlRecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GirlRecipeApp.swift
+//  GirlRecipe
+//
+//  Created by Артём Коротков on 06.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct GirlRecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
